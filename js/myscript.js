896034f21fd57f9b98fe6554340446f8f640e557
Laura Stephens <Laura.Stephens@utexas.edu>
2017-01-11 16:01:05 -0600

    fixed erase opacity bug

--- js/myscript.js
+++ js/myscript.js
@@ -7,27 +7,42 @@
    //brush
    $(document).on("mouseenter", "td", function() {
      if (color === "random") {
-       $(this).css({"background-color": randomColor(),"opacity": "1"});
+       $(this).css({
+         "background-color": randomColor(),
+         "opacity": "1"
+       });
      } else if (color === "fade") {
        var opacity = Number($(this).css("opacity"));
        if (opacity < 1) {
          opacity = opacity + 0.1;
-         $(this).css({"background-color": "black","opacity": opacity});
+         $(this).css({
+           "background-color": "black",
+           "opacity": opacity
+         });
        }
+     } else if (color === "white") {
+       $(this).css({
+         "background-color": "rgba(255,255,255,0)",
+         "opacity": "0"
+       });
+
      } else {
-      $(this).css({"background-color": color,"opacity": "1"});
+       $(this).css({
+         "background-color": "black",
+         "opacity": "1"
+       });
      }
-     
+
    });
 
    //New Grid
    $("#new").click(function() {
      gridSize = prompt("Pick a number between 16 and 40");
-     if (gridSize >= 16 && gridSize <=40){
-     $("table").empty();
-     makeGrid(gridSize);
-     color = "black";
-     }else{
+     if (gridSize >= 16 && gridSize <= 40) {
+       $("table").empty();
+       makeGrid(gridSize);
+       color = "black";
+     } else {
        alert("You didn't pick a valid number between 16 and 40. Try again.");
      }
    });
